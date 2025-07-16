b0c582d63dc17a1894193d448861360422bf714a
Raphael Bertalia <k2.raphael@grupofleury.com.br>
2025-07-16 12:53:14 -0300

    fix: server cjs

--- server.cjs
+++ server.cjs
@@ -1,16 +1,17 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 
-// Serve os arquivos da pasta "dist"
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve arquivos estáticos da pasta 'dist'
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Para qualquer rota, envia o index.html
+// Rota fallback (SPA)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor frontend rodando na porta ${PORT}`);
 });
