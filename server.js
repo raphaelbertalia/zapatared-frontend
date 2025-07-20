ebc0b008bcf3468b92ad487815f6bca3dd307eba
Raphael Bertalia <k2.raphael@grupofleury.com.br>
2025-07-19 23:57:19 -0300

    ajuste server

--- server.js
+++ server.js
@@ -6,10 +6,8 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve a pasta dist
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Rota fallback para SPA
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
