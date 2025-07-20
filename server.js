b172d8f5b8a079e9304b867391f7c775700eab53
Raphael Bertalia <k2.raphael@grupofleury.com.br>
2025-07-20 01:49:16 -0300

    ajustes docker

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+app.get('/', (req, res) => res.send('Servidor OK'));
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
